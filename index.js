a42a07e4ea3118b2af94f376149b3af50c4bce16
sumiit-jaiswal <sumitjaiswal263@gmail.com>
2024-08-05 19:02:33 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 5000; // Change this to any other available port
+const port = process.env.PORT || 3000; // Change this to any other available port
 ;
 
 // Route for the root path
