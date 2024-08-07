77fe6d7d52af612e0d7ed6ed29989e3f0687f7e3
sumiit-jaiswal <sumitjaiswal263@gmail.com>
2024-08-07 21:22:31 +0530

    jobnotification api

--- index.js
+++ index.js
@@ -11,13 +11,30 @@ app.use(cors());
 
 // Route for the root path
 app.get('/', (req, res) => {
-    res.send('Welcome to the backend server! Go to /api/data to fetch data.');
+    res.send('Welcome to the backend server!');
 });
 
 // Route to fetch data from the external API
-app.get('/api/data', async (req, res) => {
+app.get('/api/jobdata', async (req, res) => {
     try {
         const apiUrl = 'https://rm.dtu.ac.in/api/company/jobPost?page=1';
+        const token = process.env.API_TOKEN;
+        const response = await axios.get(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/api/jobnotification', async (req, res) => {
+    try {
+        const apiUrl = 'https://rm.dtu.ac.in/api/student/notifications?page=1';
         const token = process.env.API_TOKEN;
         const response = await axios.get(apiUrl, {
             headers: {
